5aa08029e1711ba17d02718f2aa2e66587f30a49
Vitaliy Morzhov <voxfoot@gmail.com>
2023-02-16 22:25:07 +0500

    task1

---
+++ task1.js
@@ -0,0 +1,19 @@
+const sum = (a, b) => {
+  const result = `${(+a) + (+b)}`;
+  return result.length < 5 ? Number(result) : +Number(result).toFixed(3); 
+};
+
+const getNumberRadix = (number, radix) => {
+  const exception = () => {
+    throw new Error('Функция getNumberRadix была вызвана с некорректными параметрами.');
+  }
+
+  const currentNumber = Number(number);
+  if (currentNumber < 0 || !Number.isInteger(currentNumber)) {
+    exception();
+  }
+  if (radix < 2 || radix > 16) {
+    exception();
+  }
+  return `${currentNumber.toString(radix)}`;
+};
