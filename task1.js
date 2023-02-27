211a93c9b489f0cb1f3db12a8b50135dd2ff2658
Vox1oot <voxfoot@gmail.com>
2023-02-27 19:33:47 +0500

    Errors correction

--- task1.js
+++ task1.js
@@ -1,20 +1,18 @@
-const sum = (a, b) => {
-  const result = `${(+a) + (+b)}`;
-  return result.length < 5 ? Number(result) : +Number(result).toFixed(3); 
-};
+const sum = (a, b) => +((+a + +b).toFixed(3));
 
 const getNumberRadix = (number, radix) => {
   const exception = () => {
     throw new Error('Функция getNumberRadix была вызвана с некорректными параметрами.');
   }
 
-  const currentNumber = Number(number);
-  if (currentNumber < 0 || !Number.isInteger(currentNumber)) {
-    exception();
-  }
-  if (radix < 2 || radix > 16) {
-    exception();
-  }
-  return `${currentNumber.toString(radix)}`;
-};
+  const currentNumber = +number;
 
+  if (
+    currentNumber < 0 ||
+    !Number.isInteger(currentNumber)||
+    Number.isNaN(currentNumber) ||
+    radix < 2 ||
+    radix > 16
+  ) { exception() }
+  return currentNumber.toString(radix);
+};
