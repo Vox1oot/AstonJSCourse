b105f8128b9069475b2cf096f738a359078af369
Vox1oot <voxfoot@gmail.com>
2023-03-02 21:23:59 +0500

    task5

---
+++ task5/Car.js
@@ -0,0 +1,9 @@
+export default class Car {
+    constructor(name = 'Unknown Car') {
+        this.fuel = 0;
+        this.lowFuelConsumption = 0;
+        this.durability = 0;
+        this.speed = 0;
+        this.name = name
+    }
+};
---
+++ task5/Civilian.js
@@ -0,0 +1,10 @@
+import Car from './Car';
+
+class Civilian extends Car {
+    constructor() {
+
+    }
+};
+
+const civilian = new Civilian();
+console.log(civilian);
